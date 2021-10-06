ed7bd9f84b0eced141c8812a54d811e0329766e1
Dylan Ogle <delxhq0@gmail.com>
2021-10-06 23:57:09 +0100

    Lets just ignore this for now.

--- src/commands/Queue.ts
+++ src/commands/Queue.ts
@@ -21,43 +21,43 @@ export const Queue = new (class extends Command {
       interaction.reply({ embeds: [Track('Current Song', player.currentTrack)] })
     }
 
-    this.sendQueue(interaction, player)
+    // this.sendQueue(interaction, player)
   }
 
-  private async sendQueue(interaction: BaseCommandInteraction, player: PlayerManager, page: number = 1, interact?: BaseCommandInteraction) {
-    const pages = player.queue.length <= PAGE_SIZE ? 1 : Math.ceil(player.queue.length / PAGE_SIZE)
+  // private async sendQueue(interaction: BaseCommandInteraction, player: PlayerManager, page: number = 1, interact?: BaseCommandInteraction) {
+  //   const pages = player.queue.length <= PAGE_SIZE ? 1 : Math.ceil(player.queue.length / PAGE_SIZE)
 
-    const embed = RichEmbed(
-      'Song Queue', '',
-      player.queue.slice(page === 1 ? 0 : (page - 1) * PAGE_SIZE, page * PAGE_SIZE).map((t, i) => [
-        `${((page - 1) * PAGE_SIZE) + i + 1}. ${t.title}`, `${t.author}`, false,
-      ]),
-    ).setFooter(`Page ${page}/${pages}`)
+  //   const embed = RichEmbed(
+  //     'Song Queue', '',
+  //     player.queue.slice(page === 1 ? 0 : (page - 1) * PAGE_SIZE, page * PAGE_SIZE).map((t, i) => [
+  //       `${((page - 1) * PAGE_SIZE) + i + 1}. ${t.title}`, `${t.author}`, false,
+  //     ]),
+  //   ).setFooter(`Page ${page}/${pages}`)
 
-    if (!message) message = (await msg.channel.send({ embeds: [embed] }) as Message)
-    else {
-      message.reactions.cache.clear()
-      message = await message.edit({ embeds: [embed] })
-    }
+  //   if (!message) message = (await msg.channel.send({ embeds: [embed] }) as Message)
+  //   else {
+  //     message.reactions.cache.clear()
+  //     message = await message.edit({ embeds: [embed] })
+  //   }
 
-    if (page - 1 > 0) await message.react('⬆')
-    if (page + 1 <= pages) message.react('⬇')
+  //   if (page - 1 > 0) await message.react('⬆')
+  //   if (page + 1 <= pages) message.react('⬇')
 
-    const filter = (reaction: MessageReaction, user: User) => {
-      return ['⬆', '⬇'].includes(reaction.emoji.name) && user.id === msg.author.id
-    }
+  //   const filter = (reaction: MessageReaction, user: User) => {
+  //     return ['⬆', '⬇'].includes(reaction.emoji.name) && user.id === msg.author.id
+  //   }
 
-    message.awaitReactions({ filter, max: 1, time: 60000 }).then(async collected => {
-      const reaction = collected.first()
-      if (!reaction) return
+  //   interaction.awaitReactions({ filter, max: 1, time: 60000 }).then(async collected => {
+  //     const reaction = collected.first()
+  //     if (!reaction) return
 
-      if (reaction.emoji.name === '⬆') {
-        this.sendQueue(msg, player, page - 1, message)
-      } else if (reaction.emoji.name === '⬇') {
-        this.sendQueue(msg, player, page + 1, message)
-      }
-    })
+  //     if (reaction.emoji.name === '⬆') {
+  //       this.sendQueue(msg, player, page - 1, message)
+  //     } else if (reaction.emoji.name === '⬇') {
+  //       this.sendQueue(msg, player, page + 1, message)
+  //     }
+  //   })
 
-    return embed
-  }
+  //   return embed
+  // }
 })()
