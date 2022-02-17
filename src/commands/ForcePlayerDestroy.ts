240be621fc35aae536d8c46676c5a1a075737c15
Dylan <me@delxhq.me>
2022-02-17 08:26:59 +0000

    Discord only likes lowercase :(

--- src/commands/ForcePlayerDestroy.ts
+++ src/commands/ForcePlayerDestroy.ts
@@ -4,16 +4,15 @@ import { RichEmbed, Error } from '../util'
 
 export const ForcePlayerDestroy = new (class extends Command {
 
-  public name = 'destroyPlayer'
+  public name = 'destroyplayer'
   public description = 'Force destroy a player'
   public options: ApplicationCommandOptionData[] = [{
-    name: 'guildId',
+    name: 'guildid',
     description: 'Guild ID of the player to destroy',
     required: true,
     type: Constants.ApplicationCommandOptionTypes.STRING,
   }]
   public permissions = []
-  public voiceOnly = true
 
   public async run({ soup, interaction, options }: IRun) {
     const guildPlayer = soup.manager.players.get(options.getString('guildId'))
