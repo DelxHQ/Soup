e402ec67a6c825e8001826ffdd0a04493c583b14
Dylan <me@delxhq.me>
2022-07-18 21:14:49 +0100

    Don't check if the user is in the same voice channel as us

--- src/commands/Leave.ts
+++ src/commands/Leave.ts
@@ -6,7 +6,6 @@ export const Leave = new (class extends Command {
   public name = 'leave'
   public description = 'Stops the current track, clears the queue and leaves the voice channel.'
   public options = []
-  public voiceOnly = true
 
   public async run({ soup, interaction }: IRun) {
     const guildPlayer = soup.manager.players.get(interaction.guild.id)
